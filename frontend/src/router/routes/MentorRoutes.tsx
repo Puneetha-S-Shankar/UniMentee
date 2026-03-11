30af5fbd24e3b17924dc98e3d4ace10281e6bf2b
Rabiya Basri <rabiyabasrifaiz@gmail.com>
2026-03-11 19:37:38 +0530

    mentorroutes

--- frontend/src/router/routes/MentorRoutes.tsx
+++ frontend/src/router/routes/MentorRoutes.tsx
@@ -1,17 +1,23 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import MentorDashboard from '../../features/mentor/pages/Dashboard';
+import MenteeList from '../../features/mentor/pages/MenteeList';
+import MenteeDetail from '../../features/mentor/pages/MenteeDetail';
+import SessionNote from '../../features/mentor/pages/SessionNote';
 
 /**
  * MentorRoutes Component
- * 
+ *
  * Defines all routes accessible to mentors
  */
 export default function MentorRoutes() {
   return (
     <Routes>
       <Route index element={<Navigate to="/mentor/dashboard" replace />} />
-      <Route path="dashboard" element={<div>Mentor Dashboard</div>} />
-      <Route path="mentees" element={<div>Mentor Mentees</div>} />
-      <Route path="sessions" element={<div>Mentor Sessions</div>} />
+      <Route path="dashboard" element={<MentorDashboard />} />
+      <Route path="mentees" element={<MenteeList />} />
+      <Route path="students/:studentId" element={<MenteeDetail />} />
+      <Route path="students/:studentId/session/new" element={<SessionNote />} />
+      <Route path="sessions/new" element={<SessionNote />} />
       <Route path="*" element={<Navigate to="/mentor/dashboard" replace />} />
     </Routes>
   );
